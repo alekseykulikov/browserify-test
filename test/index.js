26b3df1ca05212c330857c1f55c0c13251d70a8d
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2014-10-24 17:55:00 +0600

    add test for --watch

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
 describe('browserify-test', function() {
   it('runs one file', function(done) {
@@ -30,6 +31,29 @@ describe('browserify-test', function() {
       expect(stdout).contain('ok 4 PhantomJS 1.9 - test-sum calculates the sum');
       expect(stdout).contain('1..4');
       expect(stdout).contain('# ok');
+      done();
+    });
+  });
+
+  it('supports --watch', function(done) {
+    var child = spawn('./bin/browserify-test', ['--watch', './test/app/test/mul.js']);
+    var timer = setTimeout(child.kill.bind(child), 8000);
+    var counter = 0;
+
+    child.stdout.on('data', function (data) {
+      if (data.toString().trim() != '2/2 ✔') return;
+      if (counter === 0) {
+        counter = 1;
+        exec('touch ./test/app/index.js');
+      } else if (counter === 1) {
+        counter = 2;
+        child.stdin.write('q'); // close testem
+      }
+    });
+
+    child.on('close', function() {
+      clearTimeout(timer);
+      expect(counter).equal(2);
       done();
     });
   });
