2cb58a6773cd21b3de17fe34923846324586c8ed
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-09-13 13:18:58 +0300

    compile project with babel to ./src folder

--- test/index.js
+++ test/index.js
@@ -1,70 +1,60 @@
-var expect = require('chai').expect;
-var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
+import { expect } from 'chai'
+import { exec } from 'child_process'
+import { spawn } from 'child_process'
 
-describe('browserify-test', function() {
-  it('runs one file', function(done) {
-    exec('./bin/browserify-test ./test/app/test/sum.js', function(err, stdout) {
-      if (err) return done(err);
-      expect(stdout).contain('test-sum calculates the sum');
-      expect(stdout).contain('1..1');
-      expect(stdout).contain('# ok');
-      done();
-    });
-  });
+describe('browserify-test', () => {
+  it('runs one file', (done) => {
+    exec('./src/cli.js -t babelify ./test/app/test/sum.js', (err, stdout) => {
+      if (err) return done(err)
+      expect(stdout).contain('test-sum calculates the sum')
+      expect(stdout).contain('1..1')
+      expect(stdout).contain('# ok')
+      done()
+    })
+  })
 
-  it('runs many files', function(done) {
-    exec('./bin/browserify-test ./test/app/test/sum.js ./test/app/test/odd.js', function(err, stdout) {
-      if (err) return done(err);
-      expect(stdout).contain('1..2');
-      expect(stdout).contain('# ok');
-      done();
-    });
-  });
+  it('runs many files', (done) => {
+    exec('./src/cli.js -t babelify ./test/app/test/sum.js ./test/app/test/odd.js', (err, stdout) => {
+      if (err) return done(err)
+      expect(stdout).contain('1..2')
+      expect(stdout).contain('# ok')
+      done()
+    })
+  })
 
-  it('supports globs', function(done) {
-    exec('./bin/browserify-test ./test/app/test/*.js', function(err, stdout) {
-      if (err) return done(err);
-      expect(stdout).contain('test-mul calculates the mul');
-      expect(stdout).contain('test-mul supports many arguments');
-      expect(stdout).contain('test-odd calculates the odd');
-      expect(stdout).contain('test-sum calculates the sum');
-      expect(stdout).contain('1..4');
-      expect(stdout).contain('# ok');
-      done();
-    });
-  });
+  it('supports globs', (done) => {
+    exec('./src/cli.js -t babelify ./test/app/test/*.js', (err, stdout) => {
+      if (err) return done(err)
+      expect(stdout).contain('test-mul calculates the mul')
+      expect(stdout).contain('test-mul supports many arguments')
+      expect(stdout).contain('test-odd calculates the odd')
+      expect(stdout).contain('test-sum calculates the sum')
+      expect(stdout).contain('1..4')
+      expect(stdout).contain('# ok')
+      done()
+    })
+  })
 
-  it('supports --watch', function(done) {
-    var child = spawn('./bin/browserify-test', ['--watch', './test/app/test/mul.js']);
-    var timer = setTimeout(child.kill.bind(child), 8000);
-    var counter = 0;
+  it('supports --watch', (done) => {
+    const child = spawn('./src/cli.js', ['-t', 'babelify', '-w', './test/app/test/mul.js'])
+    const timer = setTimeout(child.kill.bind(child), 16000)
+    let counter = 0
 
-    child.stdout.on('data', function (data) {
-      if (data.toString().trim() != '2/2 ✔') return;
+    child.stdout.on('data', (data) => {
+      if (data.toString().trim() != '2/2 ✔') return
       if (counter === 0) {
-        counter = 1;
-        exec('touch ./test/app/index.js');
+        counter = 1
+        exec('touch ./test/app/index.js')
       } else if (counter === 1) {
-        counter = 2;
-        child.stdin.write('q'); // close testem
+        counter = 2
+        child.stdin.write('q') // close testem
       }
-    });
+    })
 
-    child.on('close', function() {
-      clearTimeout(timer);
-      expect(counter).equal(2);
-      done();
-    });
-  });
-
-  it('supports --transform', function(done) {
-    exec('./bin/browserify-test ./test/app/test2/*.js -t babelify', function(err, stdout) {
-      if (err) return done(err);
-      expect(stdout).contain('test2-sum calculates the sum');
-      expect(stdout).contain('1..1');
-      expect(stdout).contain('# ok');
-      done();
-    });
-  });
-});
+    child.on('close', () => {
+      clearTimeout(timer)
+      expect(counter).equal(2)
+      done()
+    })
+  })
+})
